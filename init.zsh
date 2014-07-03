026a09a5ffa0cf08824b31f6119620930103107c
David Mohl <me@dave.cx>
2014-07-03 17:18:32 +0900

    add init.zsh

---
+++ init.zsh
@@ -0,0 +1,10 @@
+# Vim's text-objects-ish for zsh.
+
+# Author: Takeshi Banse <takebi@laafc.net>
+# License: Public Domain
+
+# Thank you very much, Bram Moolenaar!
+# I want to use the Vim's text-objects in zsh.
+
+source opp.zsh
+source opp/*.zsh
